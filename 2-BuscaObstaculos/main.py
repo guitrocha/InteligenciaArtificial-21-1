1ce977b5436e243f09b42f6ff0e8f9d6dfd5aa29
Guilherme Rocha <guitfrocha@gmail.com>
2021-10-16 11:24:10 -0300

    feat/map generator

--- 2-BuscaObstaculos/main.py
+++ 2-BuscaObstaculos/main.py
@@ -1,30 +1,58 @@
+import numpy as np
+
 from Agent import AgentBFS, AgentDFS
 from Environment import Environment
 
+
+def generate_map(tam, prob):
+    map = np.zeros((tam, tam))
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            map[i][j] = 1 if np.random.random() < prob else 0
+
+    map[0][0] = 0
+    map[tam - 1][tam - 1] = 0
+    print(map)
+    return map
+
+
+def show_result(agent_type, path):
+    print(f"~~~ {agent_type} ~~~~~~~~~~~~~~~")
+    print(f"Path: {path}")
+    print(f"Size: {len(path)}")
+    print("================================================")
+
+
 if __name__ == "__main__":
 
-    map = [
+    tam = 8
+    prob = 1
+
+    map = generate_map(tam, prob)
+    """map = [
         [0, 0, 1],
         [1, 0, 1],
         [1, 0, 0],
-    ]
+    ]"""
+
+    start = [0, 0]
+    goal = [tam - 1, tam - 1]
 
     # Busca em largura BFS
-    # Busca em profundidade DFS
-    # Guloso
-    # Menor custo primeiro
-    # A*
-    # Branch and Bound
-    env = Environment(map, [0, 0], [2, 2])
-    ag = AgentDFS(env)
-    path = ag.act()
-    print(path)
-
-    env = Environment(map, [0, 0], [2, 2])
+    env = Environment(map, start, goal)
     ag = AgentBFS(env)
     path = ag.act()
-    print(path)
+    show_result("BFS", path)
 
+    # Busca em profundidade DFS
+    env = Environment(map, start, goal)
+    ag = AgentDFS(env)
+    path = ag.act()
+    show_result("DFS", path)
+    # Guloso
 
-def generate_map():
-    pass
+    # Menor custo primeiro
+
+    # A*
+
+    # Branch and Bound
