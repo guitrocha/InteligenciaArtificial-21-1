34a4125b7646e655fc4d8430309bbfefb35c95c6
Guilherme Rocha <guitfrocha@gmail.com>
2021-11-18 22:04:12 -0300

    feat:lista 2

--- 2-BuscaObstaculos/Environment.py
+++ 2-BuscaObstaculos/Environment.py
@@ -1,58 +1,10 @@
-import numpy as np
-
-
 class Environment:
-    def __init__(self, map, start, goal):
-        self.map = np.array(map)
-        self.start = np.array(start)
-        self.goal = np.array(goal)
-        self.agent_position = np.array(start)
-        self.actions = np.array(
-            [
-                [1, 0],
-                [-1, 0],
-                [0, 1],
-                [0, -1],
-                [1, 1],
-                [-1, 1],
-                [1, -1],
-                [-1, -1],
-            ]
-        )
+    """
+    This class implements and interface for an Environment
+    """
 
     def initial_percepts(self):
-        available = []
-        for a in self.actions:
-            pos = self.start + a
-            if (
-                (0 <= pos[0] < self.map.shape[0])
-                and (0 <= pos[1] < self.map.shape[1])
-                and self.map[pos[0]][pos[1]] == 0
-            ):
-                available.append(pos)
-
-        return {
-            "available_positions": available,
-            "position": self.agent_position,
-            "goal": self.goal,
-        }
+        raise NotImplementedError("initial_percepts")
 
     def signal(self, action):
-
-        self.agent_position = action["go_to"]
-
-        available = []
-        for a in self.actions:
-            pos = self.agent_position + a
-            if (
-                (0 <= pos[0] < self.map.shape[0])
-                and (0 <= pos[1] < self.map.shape[1])
-                and self.map[pos[0]][pos[1]] == 0
-            ):
-                available.append(pos)
-
-        return {
-            "available_positions": available,
-            "position": self.agent_position,
-            "goal": self.goal,
-        }
+        raise NotImplementedError("signal")
--- 2-BuscaObstaculos/main.py
+++ 2-BuscaObstaculos/main.py
@@ -1,74 +1,15 @@
-import numpy as np
+from PathFinderAgent import *
+from Room import Room
 
-from Agent import AgentBFS, AgentDFS, AgentGuloso
-from Environment import Environment
+# env = Room(room=[[0, 0, 1], [0, 0, 1], [0, 0, 0]], target=[2, 2])
+env = Room(prob=0.1, n=20, plot_on=True)
+# agent = RandAgent(env)
+# agent = BFSAgent(env)
+# agent = DFSAgent(env)
+# agent = IDAgent(env)
+agent = AStarAgent(env)
+# agent = GreedyAgent(env)
+# agent = BBAgent(env, bound=35)
 
-
-def generate_map(tam, prob):
-    map = np.zeros((tam, tam))
-    for i in range(len(map)):
-        for j in range(len(map[0])):
-            map[i][j] = 1 if np.random.random() < prob else 0
-
-    map[0][0] = 0
-    map[tam - 1][tam - 1] = 0
-    print(map)
-    return map
-
-
-def show_result(agent_type, path):
-    print(f"~~~ {agent_type} ~~~~~~~~~~~~~~~")
-    print(f"Path: {path}")
-    print(f"Size: {len(path)}")
-    print("================================================")
-
-
-if __name__ == "__main__":
-
-    tam = 5
-    prob = 0.5
-
-    map = generate_map(tam, prob)
-    """map = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 1],
-        [0, 0, 0, 1, 0],
-    ]"""
-    """map = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 1, 1],
-        [0, 0, 1, 1, 1, 0],
-    ]"""
-
-    start = [0, 0]
-    goal = [tam - 1, tam - 1]
-
-    # Busca em largura BFS
-    env = Environment(map, start, goal)
-    ag = AgentBFS(env)
-    path = ag.act()
-    show_result("BFS", path)
-
-    # Busca em profundidade DFS
-    env = Environment(map, start, goal)
-    ag = AgentDFS(env)
-    path = ag.act()
-    show_result("DFS", path)
-
-    # Guloso
-    env = Environment(map, start, goal)
-    ag = AgentGuloso(env)
-    path = ag.act()
-    show_result("Guloso", path)
-
-    # Menor custo primeiro
-
-    # A*
-
-    # Branch and Bound
+agent.run()
+input("Press ENTER to get out of here")
